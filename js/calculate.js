10981d7041c3e2086925c24633580df9485ca562
matthiasliszt <mliszt.program@gmail.com>
2018-04-27 18:16:49 +0200

    prettified

--- js/calculate.js
+++ js/calculate.js
@@ -1,25 +1,24 @@
+function calculateInsurance() {
+    //document.getElementById('calculationResults').innerHTML="hello";
+    console.log('calculate insurance now');
 
-function calculateInsurance(){
- //document.getElementById('calculationResults').innerHTML="hello";
- console.log('calculate insurance now');
- 
- var name=document.getElementById('clientName').value;
- var age=Number(document.getElementById('clientAge').value);
- var country=document.getElementById('clientCountry').value;
- var horsepower=Number(document.getElementById('clientHorsepower').value);
-  
- localStorage.insuranceCalculated="true";
- localStorage.insureeName=name;
+    var name = document.getElementById('clientName').value;
+    var age = Number(document.getElementById('clientAge').value);
+    var country = document.getElementById('clientCountry').value;
+    var horsepower = Number(document.getElementById('clientHorsepower').value);
 
- if(country=="Austria"){
-   var insurance = horsepower * 100 / age   + 50;
-  }
-  if(country=="Germany"){
-   var insurance = horsepower * 120 / age   + 100;
-  }
-  if(country=="Italy"){
-   var insurance=horsepower*150 / (age+2)  +  150;
-  }
+    localStorage.insuranceCalculated = "true";
+    localStorage.insureeName = name;
 
- localStorage.insuranceAmount=insurance;
+    if (country == "Austria") {
+        var insurance = horsepower * 100 / age + 50;
+    }
+    if (country == "Germany") {
+        var insurance = horsepower * 120 / age + 100;
+    }
+    if (country == "Italy") {
+        var insurance = horsepower * 150 / (age + 2) + 150;
+    }
+
+    localStorage.insuranceAmount = insurance;
 }
--- js/sample.js
+++ js/sample.js
@@ -1,69 +1,62 @@
+function loadTeamData() {
+    var images = [];
+    var captions = [];
 
-function loadTeamData()
-{var images=[];
- var captions=[];
- 
- var team=JSON.parse(teamMembers);
- for(var i=0;i<team.length;++i)
-  {
-   images.push(document.createElement("img"));
-   
-   document.getElementById('ourTeam').appendChild(images[i]);
-   
-   images[i].setAttribute("id","picture"+i);
-   images[i].setAttribute("src",team[i].image);
-   
-  }
- for(var i=0;i<team.length;++i)
-  {
-   captions.push(document.createElement("div"));
-   
-   document.getElementById('ourTeam').appendChild(captions[i]);
-   
-   captions[i].setAttribute("id","caption"+i);
-   
-   var el=document.getElementById("caption"+i);
-   el.innerHTML="<p>"+team[i].name+"</p><p>"+team[i].email+"</p>";
-  }
-console.log("Team Data Loaded");
-} 
+    var team = JSON.parse(teamMembers);
+    for (var i = 0; i < team.length; ++i) {
+        images.push(document.createElement("img"));
 
-function showUpDown(){
- document.getElementById('companyHeader').style.display="block";
- document.getElementById('checkprices').style.display="block";
+        document.getElementById('ourTeam').appendChild(images[i]);
+
+        images[i].setAttribute("id", "picture" + i);
+        images[i].setAttribute("src", team[i].image);
+
+    }
+    for (var i = 0; i < team.length; ++i) {
+        captions.push(document.createElement("div"));
+
+        document.getElementById('ourTeam').appendChild(captions[i]);
+
+        captions[i].setAttribute("id", "caption" + i);
+
+        var el = document.getElementById("caption" + i);
+        el.innerHTML = "<p>" + team[i].name + "</p><p>" + team[i].email + "</p>";
+    }
+    console.log("Team Data Loaded");
+}
+
+function showUpDown() {
+    document.getElementById('companyHeader').style.display = "block";
+    document.getElementById('checkprices').style.display = "block";
 
 }
 
-function initializeButtons(){
- document.getElementById("checkPriceButton").onclick=function(){showCalculationFrame();};
+function initializeButtons() {
+    document.getElementById("checkPriceButton").onclick = function() { showCalculationFrame(); };
 }
 
-function showCalculationFrame(){
- document.getElementById("calculationFrame").style.display="block"; 
- document.getElementById("teamWrapper").style.display="none";
- document.getElementById("checkprices").style.display="none";
- document.getElementById("calculationResults").style.display="block";
+function showCalculationFrame() {
+    document.getElementById("calculationFrame").style.display = "block";
+    document.getElementById("teamWrapper").style.display = "none";
+    document.getElementById("checkprices").style.display = "none";
+    document.getElementById("calculationResults").style.display = "block";
 }
 
-function eraseLocalStorage(){
- localStorage.setItem("insuranceCalculated","false");
- localStorage.setItem("insuranceAmount","0");
- localStorage.setItem("insureeName","No one");
+function eraseLocalStorage() {
+    localStorage.setItem("insuranceCalculated", "false");
+    localStorage.setItem("insuranceAmount", "0");
+    localStorage.setItem("insureeName", "No one");
 }
 
-function checkCalculatedInsurance(){
- if(localStorage.insuranceCalculated=="true")
-  {return true;}
- else
-  {return false;}
+function checkCalculatedInsurance() {
+    if (localStorage.insuranceCalculated == "true") { return true; } else { return false; }
 }
 
-function showInsuranceCalculation(){
- if(checkCalculatedInsurance())
-  {var el=document.getElementById('calculationResults');
-   var amount=Math.round(localStorage.insuranceAmount*100)/100;
-   var text=localStorage.insureeName+", your insurance costs "+amount+" &euro;.";
-   el.innerHTML="<p>&nbsp;</p><center>"+text+"<p>&nbsp</p>";
-  }
+function showInsuranceCalculation() {
+    if (checkCalculatedInsurance()) {
+        var el = document.getElementById('calculationResults');
+        var amount = Math.round(localStorage.insuranceAmount * 100) / 100;
+        var text = localStorage.insureeName + ", your insurance costs " + amount + " &euro;.";
+        el.innerHTML = "<p>&nbsp;</p><center>" + text + "<p>&nbsp</p>";
+    }
 }
-
